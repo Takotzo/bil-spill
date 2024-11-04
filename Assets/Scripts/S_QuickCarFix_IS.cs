485e0170d5fe1786792bb5cf3d781572808e27a5
masterITS <ingesela@gmail.com>
2024-11-04 15:27:29 +0100

    Fixed a wierd bug with the car

---
+++ Assets/Scripts/S_QuickCarFix_IS.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+public class S_QuickCarFix_IS : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position = new Vector3(0, 1, 0);
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
