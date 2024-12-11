f117a93781c7ae45003eff767ccb4eff5cb51d06
Takotzo <oyvindroberg@hotmail.no>
2024-12-11 13:17:50 +0100

    started spawnpoint script

--- Assets/Scripts/Core/SpawnPoint.cs
+++ Assets/Scripts/Core/SpawnPoint.cs
@@ -8,15 +8,19 @@ namespace Core
     {
         private static List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
     
+        private static int count = 0;
     
         public static Vector3 GetRandomSpawnPos()
         {
             if (spawnPoints.Count == 0)
             {
                 return Vector3.zero;
-            }
-        
-            return spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position;
+            } 
+            
+            Vector3 point = spawnPoints[count].transform.position;            
+            count++;
+
+            return point;
         }
 
         private void OnDrawGizmos()
